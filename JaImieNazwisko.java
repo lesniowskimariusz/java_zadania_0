badf39a85cc82a326b33010eb032d8c4b7784042
LesniowskiMariusz <lesniowski.mar@web.de>
2019-04-12 19:08:16 +0200

    Pierwszy commit z wiersza - program print tekst

---
+++ JaImieNazwisko.java
@@ -0,0 +1,9 @@
+package java_zadania_0;
+
+public class JaImieNazwisko {
+
+    public static void main(String[] args){
+
+        System.out.println("Ja" + " " + "Mariusz" + " " + "Lesniowski");
+    }
+}
